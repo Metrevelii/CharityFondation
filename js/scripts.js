d382e2892816f318f7db1b65267f6e2e4b17d271
Metreveli <metreveli2014@gmail.com>
2021-02-24 18:00:58 +0400

    added responsive and other styles, also fonts and mixins

--- js/scripts.js
+++ js/scripts.js
@@ -3,3 +3,25 @@ var navBar = document.querySelector('.nav-bar');
 toggleButton.addEventListener('click', function () {
 	navBar.classList.toggle('toggle');
 });
+
+
+
+// switch btn
+
+document.querySelector('.switch-btn').addEventListener('click', function() {
+	document.querySelector('.btn--eng').classList.add('btn--active');
+	document.querySelector('.btn--geo').classList.remove('btn--active');
+});
+
+document.querySelector('.btn--geo').addEventListener('click', function() {
+	document.querySelector('.btn--geo').classList.add('btn--active');
+	document.querySelector('.btn--eng').classList.remove('btn--active');
+})
+
+
+
+
+// multi language
+
+
+
